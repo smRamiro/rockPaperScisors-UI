0d219d5acbf71706ad1eead45ba7878b8b4585c0
sRamiro <ramiro_sando@outlook.com>
2020-04-20 12:58:45 -0300

    5 rounds loop added

--- scripts/rock-paper-scisors.js
+++ scripts/rock-paper-scisors.js
@@ -1,40 +1,81 @@
-let user1Selection = prompt('Player 1, please input your selection!').toLowerCase();
-let user2Selection = prompt('Player 2, please input your selection!').toLowerCase();
+let count = 1;
+let player1Score = 0;
+let player2Score = 0;
 
-console.log(`user 1 selection: ${user1Selection}, user2 selected: ${user2Selection}`);
-
-function rockPaperScisors(player1Selection, player2Selection){
-
-    switch(player1Selection){
-        case "rock":
-            if(player2Selection == "rock"){
-                return 'It\'s a tie';
-            }else if(player2Selection == "paper"){
-                return `Player 2 wins; ${player2Selection} DESTROYS ${user1Selection}`;
-            }else{
-                return `Player1 wins!; ${user1Selection} DESTROYS ${player2Selection}`;
-            }
-
-        case "paper":
-            if(player2Selection == "rock"){
-                return `Player1 wins!; ${user1Selection} DESTROYS ${player2Selection}`;
-                
-            }else if(player2Selection == "paper"){
-                return 'It\'s a tie';
-
-            }else if (player2Selection == "scisors"){
-                return `Player 2 wins; ${player2Selection} DESTROYS ${user1Selection}`;
-            }
-
-        case "scisors":
-            if(player2Selection == "rock"){
-                `Player 2 wins; ${player2Selection} DESTROYS ${user1Selection}`;
-            }else if(player2Selection == "paper"){
-                return `Player1 wins!; ${user1Selection} DESTROYS ${player2Selection}`;
-            }else{
-                return 'It\'s a tie';
-            }
-    }
+while (count<=5) {
+    game();
+    count++;
 }
 
-console.log(rockPaperScisors(user1Selection, user2Selection));
+function game(){
+
+    let user1Selection = prompt('Player 1, please input your selection!').toLowerCase();
+    let user2Selection = prompt('Player 2, please input your selection!').toLowerCase();
+    
+    console.log(rockPaperScisors(user1Selection, user2Selection));
+
+    function checkWinner(){
+        if(player1Score > player2Score){
+            return `Player1 is the winner!`;
+        }else if(player1Score < player2Score){
+            return `Player 2 is the winner!`;
+        }else{
+            return`There\'s no winner; it\'s a draw`;
+        }
+    }
+
+    function rockPaperScisors(player1Selection, player2Selection){
+
+        switch(player1Selection){
+            case "rock":
+
+                if(count==5){
+                    console.log(checkWinner());
+                }
+
+                if(player2Selection == "rock"){
+                    return `It\'s a tie. Score p1: ${player1Score} Score p2: ${player2Score}`;
+                }else if(player2Selection == "paper"){
+                    player2Score++;
+                    return `Player 2 wins; ${player2Selection} DESTROYS ${user1Selection}. Score p1: ${player1Score} Score p2: ${player2Score}`;
+                }else{
+                    player1Score++;
+                    return `Player1 wins!; ${user1Selection} DESTROYS ${player2Selection}. Score p1: ${player1Score} Score p2: ${player2Score}`;
+                }
+    
+            case "paper":
+
+                if(count==5){
+                    console.log(checkWinner());
+                }
+                
+                if(player2Selection == "rock"){
+                    player1Score++;
+                    return `Player1 wins!; ${user1Selection} DESTROYS ${player2Selection}. Score p1: ${player1Score} Score p2: ${player2Score}`;
+                    
+                }else if(player2Selection == "paper"){
+                    return `It\'s a tie. Score p1: ${player1Score} Score p2: ${player2Score}`;
+    
+                }else if (player2Selection == "scisors"){
+                    player2Score++;
+                    return `Player 2 wins; ${player2Selection} DESTROYS ${user1Selection}. Score p1: ${player1Score} Score p2: ${player2Score}`;
+                }
+    
+            case "scisors":
+
+                if(count==5){
+                    console.log(checkWinner());
+                }
+
+                if(player2Selection == "rock"){
+                    player2Score++;
+                    return `Player 2 wins; ${player2Selection} DESTROYS ${user1Selection}. Score p1: ${player1Score} Score p2: ${player2Score}`;
+                }else if(player2Selection == "paper"){
+                    player1Score++;
+                    return `Player1 wins!; ${user1Selection} DESTROYS ${player2Selection}. Score p1: ${player1Score} Score p2: ${player2Score}`;
+                }else{
+                    return `It\'s a tie. Score p1: ${player1Score} Score p2: ${player2Score}`;
+                }
+        }
+    }
+}
